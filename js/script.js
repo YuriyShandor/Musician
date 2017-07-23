887121807945a518f05cdf4ffa602564699b1723
YuriyShandor <yuriyshandor@gmail.com>
2017-07-23 13:33:42 +0300

    Add Section Slider

--- js/script.js
+++ js/script.js
@@ -2,4 +2,28 @@ $(document).ready(function(){
   $('.navbar-btn').on('click', function() {
     $('.navbar-inner').toggleClass('in');
   });
+
+  $('.slider').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   nextArrow: $("#right_arrow"),
+    prevArrow: $("#left_arrow"),
+   focusOnSelect: true,
+   centerMode: true,
+   centerPadding: true,
+   initialSlide: 3,
+   dots: true,
+   dotsClass: "my_slick-dots",
+   responsive: [
+     {
+       breakpoint: 769,
+
+       settings: {
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         infinite: true,
+       }
+     }
+   ]
+ });
 });
